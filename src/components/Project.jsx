517a4856c789a996e3d53f723622a347593df559
Lucy Swett <lucy.swett@alum.lehigh.edu>
2023-10-25 15:29:05 -0400

    edited project path

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,7 +8,7 @@ class Project extends React.Component {
             <div className="col-md-6 col-lg-4 project">
                 {/* <a target="_blank" rel="noopener noreferrer" href={url}> */}
                 {/* <Link className="btn-theme px-3" to="/contact">Let's Talk</Link> */}
-                <a href={`portfolio-app-2/${path}`}>
+                <a href={`/portfolio-app-2/${path}`}>
                     <img className="project-img" alt={image} src={image} />
                     <div className="overlay">
                         <div className="text font-weight-bold">{title}</div>
